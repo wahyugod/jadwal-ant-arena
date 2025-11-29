37e318589bd468bff9d974cf4bc8ec8b543d216f
Amiya-24 <flashfazz@gmail.com>
2025-11-29 18:45:40 +0800

    justify edit tim

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -445,7 +445,7 @@ $result = $conn->query($sql);
 
     <!-- Modal Edit Tim -->
     <div class="modal fade" id="editTeamModal" tabindex="-1" aria-labelledby="editTeamModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="editTeamModalLabel">
